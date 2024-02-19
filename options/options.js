e0afafcd191cecc580bf437df6f1d85378994721
Crowdbotics <team@crowdbotics.com>
2024-02-19 17:22:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ai-wound-treatment-46469",
+  url: "https://ai-wound-treatment-46469.botics.co",
+  api: "https://ai-wound-treatment-46469.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
